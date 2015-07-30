f913a02164313ae568d822e78051fe4b6ab9b2f4
hemanthuppara <hemanthuppara@gmail.com>
2015-07-30 18:39:36 +0000

    Added authentication call using rest client to get token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :check_authentication
+  
+  def check_authentication
+    session[:token] ? true : authenticate
+  end 
+  
+  def authenticate
+    response = RestClient.post I18n.t('canvas_url')+I18n.t('authenticate') , {accept: 'json'}
+    p response 
+    hash = JSON.parse(response.body)
+    if hash.has_key? "token"
+      session[:token] = hash["token"]
+      return true
+    end 
+    return false 
+  end 
+  
 end
